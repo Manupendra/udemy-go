ecd707d8140f2a977a926416f1efdbe995eb5e7c
Manupendra <manupendratiwari@gmail.com>
2026-04-25 22:26:47 +0530

    Custom Writer

--- http/main.go
+++ http/main.go
@@ -7,12 +7,20 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	resp, error := http.Get("http://google.com")
 	if error != nil {
 		fmt.Println("Error:", error)
 		os.Exit(1)
 	}
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+}
 
-	io.Copy(os.Stdout, resp.Body)
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
 }
